a08f5ce47436c615c03138bf6a9c0fee98f0eb78
Joe Rutkin <rutkinj@gmail.com>
2023-02-05 15:12:01 -0800

    finer control for combat vs movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,42 +11,41 @@ namespace RPG.Control
   {
     void Update()
     {
-      InteractWithCombat();
-      InteractWithMovement();
+      if (InteractWithCombat()) return;
+      if (InteractWithMovement()) return;
+      print("nothin");
     }
 
-    private void InteractWithCombat()
+    private bool InteractWithCombat()
     {
-      if (Input.GetMouseButtonDown(0))
+      RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+      foreach (RaycastHit hit in hits)
       {
-
-        RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
-        foreach (RaycastHit hit in hits)
+        if (hit.transform.GetComponent<CombatTarget>())
         {
-          if (hit.transform.GetComponent<CombatTarget>())
+          if (Input.GetMouseButtonDown(0))
           {
             GetComponent<Fighter>().Attack();
           }
+          return true;
         }
       }
+      return false;
     }
 
-    private void InteractWithMovement()
-    {
-      if (Input.GetMouseButton(0))
-      {
-        MoveToCursor();
-      }
-    }
-
-    public void MoveToCursor()
+    private bool InteractWithMovement()
     {
       RaycastHit hit;
       bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
       if (hasHit)
       {
-        GetComponent<Mover>().MoveTo(hit.point);
+        if (Input.GetMouseButton(0))
+        {
+          GetComponent<Mover>().MoveTo(hit.point);
+        }
+        return true;
       }
+      return false;
     }
 
     private static Ray GetMouseRay()
