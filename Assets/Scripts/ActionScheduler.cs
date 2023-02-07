67d7cae1eba8d4f932559e177d1682b8c580ade1
Joe Rutkin <rutkinj@gmail.com>
2023-02-07 08:39:22 -0800

    begin implementing ACtionScheduler

---
+++ Assets/Scripts/ActionScheduler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+  public class ActionScheduler : MonoBehaviour
+  {
+    public void StartAction(MonoBehaviour action){
+        Debug.Log("Cancel");
+    }
+  }
+}
