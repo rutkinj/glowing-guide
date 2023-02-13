0b6d96c49d25a5b5fed9ec8a5b70260593aaf671
Joe Rutkin <rutkinj@gmail.com>
2023-02-12 16:07:38 -0800

    improve portal conncetions, start scene fadeout

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,13 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
   public class Portal : MonoBehaviour
   {
+    enum DestinationId
+    {
+      A, B, C, D, E
+    }
     [SerializeField] int sceneIndex = -1;
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] DestinationId destinationId;
     private void OnTriggerEnter(Collider other)
     {
       if (other.tag == "Player")
@@ -20,8 +28,32 @@ namespace RPG.SceneManagement
     {
       DontDestroyOnLoad(gameObject);
       yield return SceneManager.LoadSceneAsync(sceneIndex);
-      print("Scene loaded");
+
+      Portal otherPortal = GetOtherPortal();
+      UpdatePlayer(otherPortal);
+
       Destroy(gameObject);
+    }
+
+    private void UpdatePlayer(Portal otherPortal)
+    {
+      GameObject player = GameObject.FindWithTag("Player");
+      player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+      player.transform.rotation = otherPortal.spawnPoint.rotation;
+    }
+
+    private Portal GetOtherPortal()
+    {
+      Portal[] portals = FindObjectsOfType<Portal>();
+      foreach (Portal portal in portals)
+      {
+        if (portal == this) continue;
+        if (portal.destinationId != destinationId)
+        {
+          return portal;
+        }
+      }
+      return null;
     }
   }
 }
