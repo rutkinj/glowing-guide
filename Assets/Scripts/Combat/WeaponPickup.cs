732eb32ab8fe6d1bc6cd66f4c0d2207b5adb6e05
Joe Rutkin <rutkinj@gmail.com>
2023-02-21 15:12:37 -0800

    adds pickup respawns

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -6,12 +6,26 @@ namespace RPG.Combat{
   public class WeaponPickup : MonoBehaviour
   {
     [SerializeField] Weapon weapon = null;
+    [SerializeField] float repawnTime = 5f;
 
     private void OnTriggerEnter(Collider other) {
         if(other.CompareTag("Player")){
             other.GetComponent<Fighter>().EquipWeapon(weapon);
-            Destroy(gameObject);
+            StartCoroutine(HideForSeconds(repawnTime));
         }
+    }
+
+    private IEnumerator HideForSeconds(float timeToWait){
+      GetComponent<CapsuleCollider>().enabled = false;
+      foreach(Transform child in transform){
+        child.gameObject.SetActive(false);
+      }
+      yield return new WaitForSeconds(timeToWait);
+      GetComponent<CapsuleCollider>().enabled = true;
+      foreach (Transform child in transform)
+      {
+        child.gameObject.SetActive(true);
+      }
     }
   }
 }
