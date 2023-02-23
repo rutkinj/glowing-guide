3b140d7dfa6465f278015edcc4ad678c91d1aff9
Joe Rutkin <rutkinj@gmail.com>
2023-02-22 16:59:15 -0800

    invert namespace dependency in enemyHPdisplay

--- Assets/Scripts/Combat/EnemyHealthDisplay.cs
+++ Assets/Scripts/Combat/EnemyHealthDisplay.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using RPG.Combat;
+using RPG.Attributes;
 
-namespace RPG.Attributes
+namespace RPG.Combat
 {
   public class EnemyHealthDisplay : MonoBehaviour
   {
