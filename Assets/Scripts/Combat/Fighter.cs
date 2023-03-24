18a4edc990deb94bf8f015882669a3f94301b050
Joe Rutkin <rutkinj@gmail.com>
2023-03-24 09:40:09 -0700

    org and comment

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -10,21 +10,22 @@ using Newtonsoft.Json.Linq;
 
 namespace RPG.Combat
 {
-  public class Fighter : MonoBehaviour, IAction, IJsonSaveable
+  public class Fighter : MonoBehaviour, IAction, IJsonSaveable, IModifierProvider
   {
     [SerializeField] Transform rightHandTransform = null;
     [SerializeField] Transform leftHandTransform = null;
     [SerializeField] Weapon defaultWeapon = null;
     HealthPoints target;
     Mover mover;
-    float timeSinceLastAttack = Mathf.Infinity;
     Weapon currentWeapon = null;
+    float timeSinceLastAttack = Mathf.Infinity;
 
     private void Start()
     {
       mover = GetComponent<Mover>();
       EquipWeapon(defaultWeapon);
     }
+
     private void Update()
     {
       timeSinceLastAttack += Time.deltaTime;
@@ -42,54 +43,10 @@ namespace RPG.Combat
       }
     }
 
-    private void AttackBehavior()
-    {
-      transform.LookAt(target.transform.position);
-      if (timeSinceLastAttack >= currentWeapon.AttackDelay && !target.GetIsDead())
-      {
-        GetComponent<Animator>().ResetTrigger("cancelAttack");
-        GetComponent<Animator>().SetTrigger("attack"); //And triggers Hit() found below
-        timeSinceLastAttack = 0f;
-      }
-    }
-
-    //Animation Event
-    private void Hit()
-    {
-      if (target == null) return;
-      float damage = GetComponent<BaseStats>().GetStat(Stat.Damage);
-      if (currentWeapon.HasProjectile())
-      {
-        currentWeapon.LaunchProjectile(rightHandTransform, leftHandTransform, target, gameObject, damage);
-      }
-      else
-      {
-        
-        target.LoseHealth(gameObject, damage);
-      }
-    }
-
-    void Shoot()
-    {
-      Hit();
-    }
-
     public void Attack(GameObject combatTarget)
     {
       GetComponent<ActionScheduler>().StartAction(this);
       target = combatTarget.GetComponent<HealthPoints>();
-    }
-
-    public void Cancel()
-    {
-      GetComponent<Animator>().ResetTrigger("attack");
-      GetComponent<Animator>().SetTrigger("cancelAttack");
-      target = null;
-    }
-
-    private bool GetIsInRange()
-    {
-      return Vector3.Distance(transform.position, target.transform.position) < currentWeapon.WeaponRange;
     }
 
     public bool CanAttack(GameObject combatTarget)
@@ -107,20 +64,75 @@ namespace RPG.Combat
       weapon.Spawn(rightHandTransform, leftHandTransform, animator);
     }
 
-    public HealthPoints GetTarget(){
+    public HealthPoints GetTarget()
+    {
       return target;
     }
 
+    private void AttackBehavior()
+    {
+      transform.LookAt(target.transform.position);
+      if (timeSinceLastAttack >= currentWeapon.AttackDelay && !target.GetIsDead())
+      {
+        GetComponent<Animator>().ResetTrigger("cancelAttack");
+        GetComponent<Animator>().SetTrigger("attack");
+        timeSinceLastAttack = 0f;
+      }
+    }
+
+    private bool GetIsInRange()
+    {
+      return Vector3.Distance(transform.position, target.transform.position) < currentWeapon.WeaponRange;
+    }
+
+    ///// ### Animation Events ###
+    private void Hit()
+    {
+      if (target == null) return;
+      float damage = GetComponent<BaseStats>().GetStat(Stat.Damage);
+      if (currentWeapon.HasProjectile())
+      {
+        currentWeapon.LaunchProjectile(rightHandTransform, leftHandTransform, target, gameObject, damage);
+      }
+      else
+      {
+
+        target.LoseHealth(gameObject, damage);
+      }
+    }
+
+    private void Shoot()
+    {
+      Hit();
+    }
+
+    ///// ### Interface Implementations ###
+    ///// IAction
+    public void Cancel()
+    {
+      GetComponent<Animator>().ResetTrigger("attack");
+      GetComponent<Animator>().SetTrigger("cancelAttack");
+      target = null;
+    }
+
+    ///// IJsonSaveable
     public JToken CaptureAsJToken()
     {
       return JToken.FromObject(currentWeapon.name);
     }
 
+    ///// IJsonSaveable
     public void RestoreFromJToken(JToken state)
     {
       string weaponName = state.ToObject<string>();
       Weapon weapon = UnityEngine.Resources.Load<Weapon>(weaponName);
       EquipWeapon(weapon);
+    }
+
+    ///// IModifierProvider
+    public IEnumerable<float> GetModifiers(Stat stat)
+    {
+      throw new System.NotImplementedException();
     }
   }
 }
