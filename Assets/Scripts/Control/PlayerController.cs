5767ffa5a068668e68933ab4f8e9e37a91ec3852
Joe Rutkin <rutkinj@gmail.com>
2023-04-01 15:56:29 -0700

    limit distance for navmesh pathing

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -6,6 +6,7 @@ using RPG.Movement;
 using RPG.Combat;
 using RPG.Attributes;
 using System;
+using UnityEngine.AI;
 
 namespace RPG.Control
 {
@@ -22,6 +23,8 @@ namespace RPG.Control
 
     }
     [SerializeField] CursorMapping[] cursorMappings = null;
+    [SerializeField] float distToNavMeshTolerance = 1f;
+    [SerializeField] float maxDistNavMeshPath = 40f;
 
     private CursorMapping cachedCursorMapping;
 
@@ -63,10 +66,12 @@ namespace RPG.Control
       return false;
     }
 
-    RaycastHit[] RaycastHitsSorted(){
+    RaycastHit[] RaycastHitsSorted()
+    {
       RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
       float[] distances = new float[hits.Length];
-      for(int i = 0; i < hits.Length; i++){
+      for (int i = 0; i < hits.Length; i++)
+      {
         distances[i] = hits[i].distance;
       }
       Array.Sort(distances, hits);
@@ -86,27 +91,61 @@ namespace RPG.Control
     //   foreach (RaycastHit hit in hits)
     //   {
     //     CombatTarget target = hit.transform.GetComponent<CombatTarget>();
-        
+
     //   }
     //   return false;
     // }
 
     private bool InteractWithMovement()
     {
-      RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
-      foreach (RaycastHit hit in hits)
+      Vector3 target;
+      bool hasHit = RaycastNavMesh(out target);
+
+      if (hasHit)
       {
-        if (hit.transform.CompareTag("Ground"))
+        if (Input.GetMouseButton(0))
         {
-          if (Input.GetMouseButton(0))
-          {
-            GetComponent<Mover>().StartMoveAction(hit.point);
-          }
-          SetCursor(CursorType.move);
-          return true;
+          GetComponent<Mover>().StartMoveAction(target);
         }
+        SetCursor(CursorType.move);
+        return true;
       }
       return false;
+    }
+
+    private bool RaycastNavMesh(out Vector3 target)
+    {
+      target = new Vector3();
+
+      RaycastHit hit;
+      bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+      if (!hasHit) return false;
+
+      NavMeshHit hitSpot;
+      bool isHitNavMesh = NavMesh.SamplePosition(hit.point, out hitSpot, distToNavMeshTolerance, NavMesh.AllAreas);
+      if (!isHitNavMesh) return false;
+
+      target = hitSpot.position;
+
+      NavMeshPath path = new NavMeshPath();
+      bool hasPath = NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
+      if (!hasPath) return false;
+      if (path.status != NavMeshPathStatus.PathComplete) return false;
+      if (GetPathLength(path) > maxDistNavMeshPath) return false;
+
+      return true;
+    }
+
+    private float GetPathLength(NavMeshPath path)
+    {
+      float totalDistance = 0f;
+      if (path.corners.Length < 2) return totalDistance;
+
+      for (int i = 0; i < path.corners.Length - 1; i++)
+      {
+        totalDistance += Vector3.Distance(path.corners[i], path.corners[i + 1]);
+      }
+      return totalDistance;
     }
 
     private static Ray GetMouseRay()
@@ -116,7 +155,7 @@ namespace RPG.Control
 
     private void SetCursor(CursorType type)
     {
-      if(cachedCursorMapping.type == type) return;
+      if (cachedCursorMapping.type == type) return;
 
       cachedCursorMapping = GetCursorMapping(type);
       Cursor.SetCursor(cachedCursorMapping.texture, cachedCursorMapping.hotspot, CursorMode.Auto);
