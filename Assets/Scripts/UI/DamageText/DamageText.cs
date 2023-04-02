12af2778594dc6eab1ee59cd2c9e00931a2a511c
Joe Rutkin <rutkinj@gmail.com>
2023-04-01 17:18:08 -0700

    characters spawn instance of dmg canvas

---
+++ Assets/Scripts/UI/DamageText/DamageText.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.UI.Damage
+{
+  public class DamageText : MonoBehaviour
+  {
+    // this is basically just a tag
+  }
+}
