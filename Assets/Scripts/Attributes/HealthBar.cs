158a953579c41aef13e3e094794c4f0bce0ba703
Joe Rutkin <rutkinj@gmail.com>
2023-04-04 10:30:03 -0700

    hp bar now reappears on character revive

--- Assets/Scripts/Attributes/HealthBar.cs
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -8,12 +8,21 @@ namespace RPG.Attributes
   {
     [SerializeField] HealthPoints hpComponent = null;
     [SerializeField] RectTransform hpBar = null;
+    [SerializeField] Canvas canvas = null;
 
     void Update()
     {
-        float percent = hpComponent.GetHPPercentage() / 100;
-        if (percent == 0) gameObject.SetActive(false);
-        hpBar.localScale = new Vector3(percent, 1, 1);
+      float percent = hpComponent.GetHPPercentage() / 100;
+      if (percent == 0)
+      {
+        canvas.enabled = false;
+        return;
+      }
+      else if (percent != 0 && canvas.enabled == false)
+      {
+        canvas.enabled = true;
+      }
+      hpBar.localScale = new Vector3(percent, 1, 1);
     }
   }
 }
