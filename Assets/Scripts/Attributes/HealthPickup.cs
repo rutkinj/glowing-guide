3e271090097af654e34dc295f0a202b6863fe363
Joe Rutkin <rutkinj@gmail.com>
2023-04-05 13:41:12 -0700

    add health pickup

---
+++ Assets/Scripts/Attributes/HealthPickup.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Attributes
+{
+  public class HealthPickup : MonoBehaviour
+  {
+    [SerializeField] float respawnTime = 3f;
+    [SerializeField] float healAmount = 20f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+      if (other.CompareTag("Player"))
+      {
+        other.GetComponent<HealthPoints>().GainHealth(healAmount);
+        StartCoroutine(HideForSeconds(respawnTime));
+      }
+    }
+
+    private IEnumerator HideForSeconds(float timeToWait)
+    {
+      ShowPickup(false);
+      yield return new WaitForSeconds(timeToWait);
+      ShowPickup(true);
+    }
+
+    private void ShowPickup(bool showOrNo)
+    {
+      GetComponent<Collider>().enabled = showOrNo;
+      foreach (Transform child in transform)
+      {
+        child.gameObject.SetActive(showOrNo);
+      }
+    }
+  }
+}
+
