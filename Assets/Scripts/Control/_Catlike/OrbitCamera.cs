8e7edbdbdbcf815ba0051b6d52d6583413307a9a
Joe Rutkin <rutkinj@gmail.com>
2023-04-12 13:46:30 -0700

    simple follow dampening

--- Assets/Scripts/Control/_Catlike/OrbitCamera.cs
+++ Assets/Scripts/Control/_Catlike/OrbitCamera.cs
@@ -5,4 +5,34 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class OrbitCamera : MonoBehaviour
 {
+  [SerializeField] Transform target;
+  [SerializeField] float distance;
+  [SerializeField, Min(0f)] float focusRadius = 1f;
+  Vector3 camFocus;
+
+  private void Awake()
+  {
+    camFocus = target.position;
+  }
+
+  private void LateUpdate()
+  {
+    UpdateTargetPos();
+    Vector3 lookDir = transform.forward;
+    transform.localPosition = camFocus - lookDir * distance;
+  }
+
+  private void UpdateTargetPos()
+  {
+    Vector3 targetPos = target.position;
+    if (focusRadius > 0f)
+    {
+      float dist = Vector3.Distance(targetPos, camFocus);
+      if (dist > focusRadius)
+      {
+        camFocus = Vector3.Lerp(targetPos, camFocus, focusRadius / dist);
+      }
+    }
+    else camFocus = targetPos;
+  }
 }
