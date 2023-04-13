ced5dda2ecdb0db41820e1c0baa506fad9de3d18
Joe Rutkin <rutkinj@gmail.com>
2023-04-12 17:02:20 -0700

    manual camera rotation

--- Assets/Scripts/Control/_Catlike/OrbitCamera.cs
+++ Assets/Scripts/Control/_Catlike/OrbitCamera.cs
@@ -7,9 +7,16 @@ public class OrbitCamera : MonoBehaviour
 {
   [SerializeField] Transform target;
   [SerializeField] float distance;
+
+  [Header("Orbit")]
+  [SerializeField, Range(1f, 360f)] float rotationSpeed = 90f;
+
+  [Header("Focus")]
   [SerializeField, Min(0f)] float focusRadius = 1f;
   [SerializeField, Range(0f, 0.99f)] float centeringFactor = 0.5f;
+
   Vector3 camFocus;
+  Vector2 orbitAngles = new Vector2(45f, 0f);
 
   private void Awake()
   {
@@ -19,8 +26,12 @@ public class OrbitCamera : MonoBehaviour
   private void LateUpdate()
   {
     UpdateTargetPos();
-    Vector3 lookDir = transform.forward;
-    transform.localPosition = camFocus - lookDir * distance;
+    ManualRotation();
+
+    Quaternion lookRotation = Quaternion.Euler(orbitAngles);
+    Vector3 lookDirection = lookRotation * Vector3.forward;
+    Vector3 lookPosisition = camFocus - lookDirection * distance;
+    transform.SetPositionAndRotation(lookPosisition, lookRotation);
   }
 
   private void UpdateTargetPos()
@@ -30,16 +41,27 @@ public class OrbitCamera : MonoBehaviour
     {
       float dist = Vector3.Distance(targetPosCurrent, camFocus);
       float lerpTime = 1f;
-        if(dist > 0.01f && centeringFactor > 0f){
-            lerpTime = Mathf.Pow(1f - centeringFactor, Time.unscaledDeltaTime);
-        }
+      if (dist > 0.01f && centeringFactor > 0f)
+      {
+        lerpTime = Mathf.Pow(1f - centeringFactor, Time.unscaledDeltaTime);
+      }
 
       if (dist > focusRadius)
       {
         lerpTime = Mathf.Min(lerpTime, focusRadius / dist);
       }
-        camFocus = Vector3.Lerp(targetPosCurrent, camFocus, lerpTime);
+      camFocus = Vector3.Lerp(targetPosCurrent, camFocus, lerpTime);
     }
     else camFocus = targetPosCurrent;
+  }
+
+  private void ManualRotation()
+  {
+    Vector2 input = new Vector2(Input.GetAxis("Vertical Camera"), Input.GetAxis("Horizontal Camera"));
+    const float deadZone = 0.001f;
+    if (input.x < -deadZone || input.x > deadZone || input.y < -deadZone || input.y > deadZone)
+    {
+      orbitAngles += rotationSpeed * Time.unscaledDeltaTime * input;
+    }
   }
 }
