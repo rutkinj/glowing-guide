bb256cbefdceedd35818fabc28418c723e1a2e18
Joe Rutkin <rutkinj@gmail.com>
2023-04-14 15:44:44 -0700

    auto zoom works with varied focus strengths

--- Assets/Scripts/Control/_Catlike/OrbitCamera.cs
+++ Assets/Scripts/Control/_Catlike/OrbitCamera.cs
@@ -62,9 +62,17 @@ public class OrbitCamera : MonoBehaviour
     Vector3 lookDirection = lookRotation * Vector3.forward;
     Vector3 lookPosition = camFocus - lookDirection * distance;
 
-    if (Physics.BoxCast(camFocus, CameraHalfExtends, -lookDirection, out RaycastHit hitInfo, lookRotation, distance - cam.nearClipPlane))
+    Vector3 rectOffset = lookDirection * cam.nearClipPlane;
+    Vector3 rectPosition = lookPosition + rectOffset;
+    Vector3 castFrom = target.position;
+    Vector3 castLine = rectPosition - castFrom;
+    float castDistance = castLine.magnitude;
+    Vector3 castDirection = castLine / castDistance;
+
+    if (Physics.BoxCast(castFrom, CameraHalfExtends, castDirection, out RaycastHit hitInfo, lookRotation, castDistance))
     {
-      lookPosition = camFocus - lookDirection * (hitInfo.distance + cam.nearClipPlane);
+      rectPosition = castFrom + castDirection * hitInfo.distance;
+      lookPosition = rectPosition - rectOffset;
     }
 
     transform.SetPositionAndRotation(lookPosition, lookRotation);
