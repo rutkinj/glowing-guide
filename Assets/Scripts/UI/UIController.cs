2cd5d9087ee10f49c2a710185b8d8134da9a816b
Joe Rutkin <rutkinj@gmail.com>
2023-05-15 16:33:41 -0700

    refactor ui for improved screen scaling

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -2,33 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIController : MonoBehaviour
+namespace RPG.UI
 {
-  [SerializeField] GameObject pauseScreen;
-  [SerializeField] GameObject characterScreen;
-  [SerializeField] GameObject debugScreen;
-
-  void Update()
+  public class UIController : MonoBehaviour
   {
-    if (Input.GetKeyDown(KeyCode.P))
-    {
-      ToggleScreen(pauseScreen);
-      if (characterScreen.activeSelf) ToggleScreen(characterScreen);
-    }
-    if (Input.GetKeyDown(KeyCode.I))
-    {
-      ToggleScreen(characterScreen);
-      if (pauseScreen.activeSelf) ToggleScreen(pauseScreen);
-    }
-    if (Input.GetKeyDown(KeyCode.O))
-    {
-      ToggleScreen(debugScreen);
-    }
-  }
+    [SerializeField] GameObject pauseScreen;
+    [SerializeField] GameObject characterScreen;
+    [SerializeField] GameObject debugScreen;
 
-  private void ToggleScreen(GameObject screen)
-  {
-    screen.SetActive(!screen.activeSelf);
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.P))
+      {
+        ToggleScreen(pauseScreen);
+        if (characterScreen.activeSelf) ToggleScreen(characterScreen);
+      }
+      if (Input.GetKeyDown(KeyCode.I))
+      {
+        ToggleScreen(characterScreen);
+        if (pauseScreen.activeSelf) ToggleScreen(pauseScreen);
+      }
+      if (Input.GetKeyDown(KeyCode.O))
+      {
+        ToggleScreen(debugScreen);
+      }
+    }
 
+    private void ToggleScreen(GameObject screen)
+    {
+      screen.SetActive(!screen.activeSelf);
+    }
   }
 }
