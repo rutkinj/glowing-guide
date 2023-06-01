24bb53530cb71eb00616b153ed196e87ee7fc8ca
Joe Rutkin <rutkinj@gmail.com>
2023-06-01 09:57:55 -0700

    very basic input with event subscription

--- Assets/InputTest/Controller.cs
+++ Assets/InputTest/Controller.cs
@@ -5,13 +5,49 @@ using UnityEngine.InputSystem;
 
 public class Controller : MonoBehaviour
 {
-    private PlayerInput playerInput;
+  private PlayerInput playerInput;
+  private InputAction jump;
 
-    [SerializeField] Material red;
+  [SerializeField] Material red;
+  [SerializeField] Material green;
+  [SerializeField] Material regular;
 
-    private void Awake() {
-        
+  private void Awake()
+  {
+    playerInput = GetComponent<PlayerInput>();
+    jump = playerInput.actions["Jump"];
+  }
+
+  private void OnEnable()
+  {
+    jump.started += Jump;
+    jump.performed += Jump;
+    jump.canceled += Jump;
+  }
+
+  private void OnDisable()
+  {
+    jump.performed -= Jump;
+  }
+
+  private void Jump(InputAction.CallbackContext ctx)
+  {
+    if (ctx.started)
+    {
+      FindObjectOfType<SphereCollider>().GetComponent<MeshRenderer>().material = green;
     }
+    else if (ctx.performed)
+    {
+      FindObjectOfType<SphereCollider>().GetComponent<MeshRenderer>().material = red;
+    }
+    else if(ctx.canceled){
+      FindObjectOfType<SphereCollider>().GetComponent<MeshRenderer>().material = regular;
 
-    
+    }
+  }
+
+  //   private void OnJump()
+  //   {
+  //     FindObjectOfType<SphereCollider>().GetComponent<MeshRenderer>().material = red;
+  //   }
 }
