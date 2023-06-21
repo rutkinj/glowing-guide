54e38fe1c33c1c47bb93ce2dba021cf1125cf812
Joe Rutkin <rutkinj@gmail.com>
2023-06-21 16:22:12 -0700

    add script for removing inv items

---
+++ Assets/Scripts/Inventories/DropTarget.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using RPG.UI.Inventories;
+using UnityEngine;
+
+namespace RPG.Inventories
+{
+  public class DropTarget : MonoBehaviour, IDragDestination<InventoryItem>
+  {
+    public void AddItems(InventoryItem item, int amount){
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<ItemDropper>().DropItem(item);
+    }
+
+    public int MaxAcceptable(InventoryItem item){
+        return int.MaxValue;
+    }
+  }
+}
