38605ee25d862bd6a07607de9334e7de6b09e341
Joe Rutkin <rutkinj@gmail.com>
2023-06-26 07:58:26 -0700

    create relevant equipment scripts

---
+++ Assets/Scripts/Inventories/EquipLocation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Inventories
+{
+  public enum EquipLocation
+  {
+    PrimaryHand,
+    OffHand,
+    Head,
+    Necklace,
+    Chest,
+    Hands,
+    Belt,
+    Legs,
+    Feet,
+    Trinket
+  }
+}
