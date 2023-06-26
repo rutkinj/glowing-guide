14fba8c165f45c92ceff0f7dbed67067ef64145a
Joe Rutkin <rutkinj@gmail.com>
2023-06-26 08:40:16 -0700

    skeletal equip slot ui structure

--- Assets/Scripts/UI/Inventory/EquipSlotUI.cs
+++ Assets/Scripts/UI/Inventory/EquipSlotUI.cs
@@ -1,21 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using RPG.Inventories;
 using UnityEngine;
 
 namespace RPG.UI.Inventories
 {
-  public class EquipSlotUI : MonoBehaviour
+  public class EquipSlotUI : MonoBehaviour, IItemHolder, IDragContainer<EquipableItem>
   {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] EquipLocation equipLocation;
+    [SerializeField] ItemIcon icon;
+    
+    public void AddItems(EquipableItem item, int number)
     {
-
+      throw new System.NotImplementedException();
     }
 
-    // Update is called once per frame
-    void Update()
+    public EquipableItem GetItem()
     {
+      throw new System.NotImplementedException();
+    }
 
+    public int GetNumber()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public int MaxAcceptable(EquipableItem item)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void RemoveItems(int number)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    InventoryItem IItemHolder.GetItem()
+    {
+      throw new System.NotImplementedException();
     }
   }
 }
