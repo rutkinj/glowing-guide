e2b102561b3a3da42f80157e3607ec5c897d0179
Joe Rutkin <rutkinj@gmail.com>
2023-06-26 17:09:34 -0700

    start on hotbar scripts; nonworking

---
+++ Assets/Scripts/Inventories/Hotbar.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using RPG.Saving;
+using UnityEngine;
+
+namespace RPG.Inventories
+{
+  public class Hotbar : MonoBehaviour, IJsonSaveable
+  {
+    Dictionary<int, HotbarSlot> hotbarItems = new Dictionary<int, HotbarSlot>();
+
+    private class HotbarSlot
+    {
+      public HotbarItem item;
+      public int itemCount;
+    }
+
+    public HotbarItem GetItem(int index)
+    {
+      if (hotbarItems.ContainsKey(index))
+      {
+        return hotbarItems[index].item;
+      }
+      return null;
+    }
+
+    public int GetNumber(int index)
+    {
+      if (hotbarItems.ContainsKey(index))
+      {
+        return hotbarItems[index].itemCount;
+      }
+      return 0;
+    }
+
+    public void AddItem(InventoryItem item, int index, int itemCount)
+    {
+      if ((HotbarItem)item == null) return;
+
+      if (hotbarItems.ContainsKey(index))
+      {
+        if (object.ReferenceEquals(item, hotbarItems[index].item))
+        {
+          hotbarItems[index].itemCount += itemCount;
+        }
+      }
+      else
+      {
+        var slot = new HotbarSlot();
+        slot.item = (HotbarItem)item;
+        slot.itemCount = itemCount;
+        hotbarItems[index] = slot;
+      }
+
+      if (hotbarUpdated != null)
+      {
+        hotbarUpdated();
+      }
+
+    }
+
+    public event Action hotbarUpdated;
+
+    public JToken CaptureAsJToken()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void RestoreFromJToken(JToken state)
+    {
+      throw new System.NotImplementedException();
+    }
+
+  }
+}
---
+++ Assets/Scripts/Inventories/HotbarItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Inventories
+{
+  public class HotbarItem : InventoryItem
+  {
+    
+  }
+}
