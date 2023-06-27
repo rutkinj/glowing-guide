84075d222d7720852aef4e3212da1d95eb6fd52f
Joe Rutkin <rutkinj@gmail.com>
2023-06-26 17:20:23 -0700

    make inventoryItem abstract

--- Assets/Scripts/Inventories/InventoryItem.cs
+++ Assets/Scripts/Inventories/InventoryItem.cs
@@ -13,7 +13,7 @@ namespace RPG.Inventories
     /// `EquipableItem`.
     /// </remarks>
     [CreateAssetMenu(menuName = ("Inventory/Item"))]
-    public class InventoryItem : ScriptableObject, ISerializationCallbackReceiver    {
+    public abstract class InventoryItem : ScriptableObject, ISerializationCallbackReceiver    {
         // CONFIG DATA
         [Tooltip("Auto-generated UUID for saving/loading. Clear this field if you want to generate a new one.")]
         [SerializeField] string itemID = null;
